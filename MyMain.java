3d3416048026260b8f1a7950fe8de0b911627e0b
Dilshan09 <121423710+Dilshan09@users.noreply.github.com>
2023-02-05 10:43:55 +0530

    Main class
    Main class for Random number

---
+++ MyMain.java
@@ -0,0 +1,14 @@
+
+public class MyMain {
+	
+	public static void main(String[]args) {
+		
+		System.out.print("\nRandom Matrix :  \n\n");
+
+		RandNum rand = new RandNum();        //Create an object for RandNum class
+	
+	    rand.random();                       //Using object call to the function
+	}
+	
+
+}
